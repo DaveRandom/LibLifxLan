40bd0a379c78b82a4d375cd5de54699a0a8319ec
Chris Wright <daverandom@php.net>
2018-01-03 18:12:08 +0000

    Add EnumTest

---
+++ tests/src/EnumTest.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+namespace DaveRandom\LibLifxLan\Tests;
+
+use DaveRandom\LibLifxLan\Enum;
+use PHPUnit\Framework\TestCase;
+
+final class TestEnum extends Enum
+{
+    public const ONE = 1;
+    public const TWO = 2;
+    public const THREE = 1;
+}
+
+final class EnumTest extends TestCase
+{
+    public function testToArray()
+    {
+        $this->assertSame(TestEnum::toArray(), ['ONE' => 1, 'TWO' => 2, 'THREE' => 1]);
+    }
+
+    public function testParseNameWithExistingName()
+    {
+        $this->assertSame(TestEnum::parseName('ONE'), 1);
+    }
+
+    public function testParseNameWithExistingNameCaseInsensitive()
+    {
+        $this->assertSame(TestEnum::parseName('one', true), 1);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testParseNameWithExistingNameCaseInsensitiveDisabled()
+    {
+        TestEnum::parseName('one');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testParseNameWithNonExistentName()
+    {
+        TestEnum::parseName('FOUR');
+    }
+
+    public function testParseValueWithExistingValue()
+    {
+        $this->assertSame(TestEnum::parseValue(1), 'ONE');
+    }
+
+    public function testParseValueWithExistingValueLoose()
+    {
+        $this->assertSame(TestEnum::parseValue('1', true), 'ONE');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testParseValueWithExistingValueLooseDisabled()
+    {
+        TestEnum::parseValue('1');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testParseValueWithNonExistentValue()
+    {
+        TestEnum::parseValue(4);
+    }
+
+    /**
+     * @expectedException \Error
+     */
+    public function testEnumCannotBeInstantiated()
+    {
+        new TestEnum;
+    }
+}
