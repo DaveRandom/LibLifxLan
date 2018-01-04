f652ea06909cd561384fb26f0a97f97c57042e46
Chris Wright <daverandom@php.net>
2018-01-04 23:02:47 +0000

    Update requirements and float representation detection logic

--- README.md
+++ README.md
@@ -12,11 +12,8 @@ WIP. Do not rely on any of this code as the API is currently subject to change.
 
 ## Requirements
 
-- PHP 7.1.1, because floating point endianness [specifiers](https://secure.php.net/pack#refsect1-function.pack-changelog)
-  for `pack()`/`unpack()` are used.
-- 64-bit PHP builds, mostly because timestamp fields use 64-bit integers with a value expressed in nanoseconds since
-  unix epoch. This requirement could be removed with some work, all that is needed is a pair of routines to convert
-  to/from a `DateTimeImmutable` object and this (somewhat odd?) integer representation.
+- A 64-bit PHP build (no, I will not be adding 32-bit support).
+- PHP >=7.1.1, or PHP 7.1.0 and a little-endian CPU.
 
 ## @todo
 
--- src/functions.php
+++ src/functions.php
@@ -2,20 +2,23 @@
 
 namespace DaveRandom\LibLifxLan;
 
-// @codeCoverageIgnoreStart
 use DaveRandom\LibLifxLan\Exceptions\InvalidValueException;
-
-if (\strlen(\pack('e', 0.1)) === 4) {
-    \define(__NAMESPACE__ . '\\FLOAT32_CODE', 'e');
-} else if (\strlen(\pack('g', 0.1)) === 4) {
-    \define(__NAMESPACE__ . '\\FLOAT32_CODE', 'g');
-} else {
-    /** @noinspection PhpUnhandledExceptionInspection */
-    throw new \Error('Cannot pack()/unpack() floating point numbers to a 32-bit little-endian representation');
-}
 
 const UINT32_MIN = 0;
 const UINT32_MAX = 0xffffffff;
+
+// @codeCoverageIgnoreStart
+foreach (['g', 'f', 'e'] as $format) {
+    if (\pack($format, -0.0) === "\x00\x00\x00\x80") {
+        \define(__NAMESPACE__ . '\\FLOAT32_CODE', $format);
+        break;
+    }
+}
+
+if (!\defined(__NAMESPACE__ . '\\FLOAT32_CODE')) {
+    /** @noinspection PhpUnhandledExceptionInspection */
+    throw new \Error('Cannot pack()/unpack() floating point numbers to a 32-bit little-endian representation');
+}
 // @codeCoverageIgnoreEnd
 
 /**
